57a99d43994e3774098fca93d84159a3b12b8a93
uranioh <melis.alessandro05@gmail.com>
2017-11-25 21:49:40 +0100

    Add files via upload

--- shitstorm.py
+++ shitstorm.py
@@ -6,78 +6,57 @@ from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, Rege
 from time import sleep
 import os
 FLOOD = range(1)
+
+
+reply_keyboard = [['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
+                      ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
+                      ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned']]
+inline_keyboard = [
+        [InlineKeyboardButton("👺", url='t.me/ouned'), InlineKeyboardButton("👺", url='t.me/ouned')],
+        [InlineKeyboardButton("👺", url='t.me/ouned'), InlineKeyboardButton("👺", url='t.me/ouned')],
+        [InlineKeyboardButton("👺", url='t.me/ouned'), InlineKeyboardButton("👺", url='t.me/ouned')],
+        [InlineKeyboardButton("👺", url='t.me/ouned'), InlineKeyboardButton("👺", url='t.me/ouned')],
+        [InlineKeyboardButton("👺", url='t.me/ouned'), InlineKeyboardButton("👺", url='t.me/ouned')],
+        [InlineKeyboardButton("👺", url='t.me/ouned'), InlineKeyboardButton("👺", url='t.me/ouned')]
+    ]
+
 def start(bot, update):
+    global reply_keyboard, inline_keyboard
     if update.message.chat.type == "private":
         update.message.reply_text("Kiao %s, aggiungimi a un gruppo!" % update.message.from_user.name)
     else:
-        try:
-            reply_keyboard = [['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
-                              ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
-                              ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned']]
-            cheibord = [
-                [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-                [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-                [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-                [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-                [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-                [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')]
-            ]
-            a = 0
-            while a < 5:
-                a = a + 1
-                bot.sendMessage(chat_id=update.message.chat_id, text="PEDDOS HANFAMS\n\n\n@ouned\n\n\n" * 100,
-                                reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False))
-                bot.sendMessage(chat_id=update.message.chat_id, text="PEDDOS HANFAMS\n\n\n@ouned\n\n\n" * 100,
-                                reply_markup=InlineKeyboardMarkup(cheibord))
-        except Exception as e:
-            print(e)
-    return flood
-def flood(bot, update):
-    reply_keyboard2 = [['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
-                      ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
-                      ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned']]
-
-    cheibord3 = [
-        [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-        [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-        [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-        [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-        [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-        [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')]
-    ]
-    a = 0
-    while a < 5:
-        a = a + 1
-        bot.sendMessage(chat_id=update.message.chat_id, text="PEDDOS HANFAMS\n\n\n@ouned\n\n\n" * 100,
-                        reply_markup=ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=False))
-        bot.sendMessage(chat_id=update.message.chat_id, text="PEDDOS HANFAMS\n\n\n@ouned\n\n\n" * 100,
-                        reply_markup=InlineKeyboardMarkup(cheibord3))
-
-def cancel(bot, update):
-    print("SOS")
-def on_join(bot, update):
-    if update.message.new_chat_members:
-        reply_keyboard4 = [['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
-                           ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned'],
-                           ['@ouned', '@ouned', '@ouned', '@ouned', '@ouned']]
-
-        cheibord4 = [
-            [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-            [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-            [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-            [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-            [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')],
-            [InlineKeyboardButton("Aced", url='t.me/ouned'), InlineKeyboardButton("Aced", url='t.me/ouned')]
-        ]
         a = 0
         while a < 5:
             a = a + 1
-            bot.sendMessage(chat_id=update.message.chat_id, text="PEDDOS HANFAMS\n\n\n@ouned\n\n\n" * 100,
-                            reply_markup=ReplyKeyboardMarkup(reply_keyboard4, one_time_keyboard=False))
-            bot.sendMessage(chat_id=update.message.chat_id, text="PEDDOS HANFAMS\n\n\n@ouned\n\n\n" * 100,
-                            reply_markup=InlineKeyboardMarkup(cheibord4))
+            bot.sendMessage(chat_id=update.message.chat_id, text="@Ouned 👺\n\n\n👺👺👺👺👺\n\n\n" * 100,
+                            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False))
+            bot.sendMessage(chat_id=update.message.chat_id, text="@ouned 👺\n\n\n👺👺👺👺👺\n\n\n" * 100,
+                            reply_markup=InlineKeyboardMarkup(inline_keyboard))
+    return flood
+def flood(bot, update):
+    global reply_keyboard, inline_keyboard
+    a = 0
+    while a < 5:
+        a = a + 1
+        bot.sendMessage(chat_id=update.message.chat_id, text="@Ouned 👺\n\n\n👺👺👺👺👺\n\n\n" * 100,
+                        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False))
+        bot.sendMessage(chat_id=update.message.chat_id, text="@ouned 👺\n\n\n👺👺👺👺👺\n\n\n" * 100,
+                        reply_markup=InlineKeyboardMarkup(inline_keyboard))
+
+def cancel():
+    print("SOS")
+def on_join(bot, update):
+    global reply_keyboard, inline_keyboard
+    if update.message.new_chat_members:
+        a = 0
+        while a < 5:
+            a = a + 1
+            bot.sendMessage(chat_id=update.message.chat_id, text="@Ouned 👺\n\n\n👺👺👺👺👺\n\n\n" * 100,
+                            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False))
+            bot.sendMessage(chat_id=update.message.chat_id, text="@ouned 👺\n\n\n👺👺👺👺👺\n\n\n" * 100,
+                            reply_markup=InlineKeyboardMarkup(inline_keyboard))
 def main():
-    updater = Updater("432123728:AAHJ1FXWZm-Msx-Rd7qo4Cur8DxYdUdqFsw")
+    updater = Updater("432123728:AAElJkRP6anMgQcq9x3eGps0-cj1FZ-EFrc")
     dp = updater.dispatcher
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
